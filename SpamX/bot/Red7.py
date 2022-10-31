b5efdf82ea28ccfb13106d73713211bd69fd4317
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2022-11-01 00:21:39 +0530

    Update Red7.py

--- SpamX/bot/Red7.py
+++ SpamX/bot/Red7.py
@@ -1,97 +1,36 @@
-import os
-from pyrogram import filters, Client
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from PhoenixScanner import Phoenix 
+import os, asyncio
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from PhoenixScanner import Phoenix
+from asyncio import get_event_loop, sleep
 
 
 RED = Phoenix(os.getenv("RED7_TOKEN"))
+SCANLIST = []
 
-def call_back_filter(data):
-    return filters.create(lambda flt, _, query: flt.data in query.data,
-                          data=data)
+async def update_list():
+   global SCANLIST
+   newlist = RED.scanlist()
+   SCANLIST = newlist
+   await sleep(60)
 
-def is_admin(group_id: int, user_id: int):
-    try:
-        user_data = Client.get_chat_member(group_id, user_id)
-        if user_data.status == 'administrator' or user_data.status == 'creator':
-            return True
-        else:
-            return False
-    except:
-        return False
+loop = get_event_loop()
+loop.create_task(update_list())
 
-@Client.on_callback_query(call_back_filter("kick"))
-def kick_callback(_, query):
-    user = query.data.split(":")[2]
-    if is_admin(query.message.chat.id,
-                query.from_user.id) and query.data.split(":")[1] == "kick":
-        Client.ban_chat_member(query.message.chat.id, user)
-        Client.unban_chat_member(query.message.chat.id, user)
-        query.answer('Kicked!')
-        query.message.edit(
-            f'Kick User [{user}](tg://user?id={user})\n Admin User [{query.from_user.id}](tg://user?id={query.from_user.id})',
-            parse_mode='markdown')
-    else:
-        message.reply('You are not admin!')
-
-
-@Client.on_callback_query(call_back_filter("ban"))
-def ban_callback(_, query):
-    user = query.data.split(":")[2]
-    if is_admin(query.message.chat.id,
-                query.from_user.id) and query.data.split(":")[1] == "ban":
-        Client.ban_chat_member(query.message.chat.id, user)
-        query.answer('Banned')
-        query.message.edit(
-            f'Banned User [{user}](tg://user?id={user})\n Admin User [{query.from_user.id}](tg://user?id={query.from_user.id})',
-            parse_mode='markdown')
-    else:
-        message.reply('You are not admin!')
-
-
-@Client.on_callback_query(call_back_filter("mute"))
-def mute_callback(_, query):
-    user = query.data.split(":")[2]
-    if is_admin(query.from_user.id,
-                query.message.chat.id) and query.data.split(":")[1] == "mute":
-        Client.restrict_chat_member(
-            query.message.chat.id,
-            user,
-            ChatPermissions(can_send_messages=False),
-        )
-        query.answer('Muted!')
-        query.message.edit(
-            f'Muted User [{user}](tg://user?id={user})\n Admin User [{query.from_user.id}](tg://user?id={query.from_user.id})',
-            parse_mode='markdown')
-    else:
-        message.reply('You are not admin!')
-
-
-@Client.on_message(filters.new_chat_members)
-async def botrm(_, m: Message): 
-  user = m.from_user.id
-  check = RED.check(user)
-  if check["is_gban"]:
-      title = m.from_user.first_name
-      msg = f"""
- Alert ⚠️
-User [{title}](tg://user?id={user}) is officially
+@Client.on_message(filters.user(SCANLIST) & filters.group & filters.all)
+async def red7xphoenix(bot: Client, message: Message):
+   user = message.from_user
+   chat = message.chat
+   user = await bot.get_users(user.id)
+   msg = f"""
+** Alert ⚠️**
+User [{user.first_name}](tg://user?id={user.id}) is officially
 Scanned by Team Red7 | Phoenix API ;)
-
-Appeal [Here](https://t.me/Red7WatchSupport) | 
-
-(Press on below button to remove this shit.Else it may put your groups, Chat in danger)
+Appeal [Here](https://t.me/Red7WatchSupport)
 """
-      await m.reply_text(msg, 
-                reply_markup=InlineKeyboardMarkup([
-                [
-                    InlineKeyboardButton("Ban",
-                                         callback_data=f"ban:ban:{user}"),
-                    InlineKeyboardButton("Kick",
-                                         callback_data=f"kick:kick:{user}"),
-                    InlineKeyboardButton("Mute",
-                                         callback_data=f"mute:mute:{user}")
-                ],
-            ]))
-  else:
-     pass
+   try:
+      await bot.ban_chat_member(chat.id, user.id)
+      await bot.send_message(chat.id, msg, disable_web_page_preview=True)
+   except:
+      await bot.send_message(chat.id, msg, disable_web_page_preview=True)
+      pass
