abbc984e6fa5eaba5e5d47c640bb38e7579c658a
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2022-10-31 15:23:52 +0530

    Create Red7.py

---
+++ SpamX/bot/Red7.py
@@ -0,0 +1,29 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from PhoenixScanner import Phoenix
+
+
+RED = Phoenix(os.getenv("RED7_TOKEN"))
+
+@Client.on_message(filters.group & filters.all)
+async def red7xphoenix(bot: Client, message: Message):
+   user = message.from_user
+   chat = message.chat
+   
+   check = RED.check(user.id)
+   if check['is_gban']:
+      user = await bot.get_users(user.id)
+      msg = f"""
+** Alert ⚠️**
+User [{user.first_name}](tg://user?id={user.id}) is officially
+Scanned by Team Red7 | Phoenix API ;)
+
+Appeal [Here](https://t.me/Red7WatchSupport)
+"""
+      try:
+         await bot.ban_chat_member(chat.id, user.id)
+         await bot.send_message(chat.id, msg, disable_web_page_preview=True)
+      except:
+         await bot.send_message(chat.id, msg, disable_web_page_preview=True)
+         pass
